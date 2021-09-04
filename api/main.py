0cde6d5b8034c131e8bf93b37111ff69bea8f61e
Reinout Holtrup <reinout91@gmail.com>
2021-09-04 22:45:35 +0200

    fixes

--- api/main.py
+++ api/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 # NEW
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://127.0.0.1:8080",
+    "http://127.0.0.1:8081",
+    "http://localhost:8080",
+    "http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

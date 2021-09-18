a82a318531775857795d2672c63e758f495483fc
Reinout Holtrup <reinout91@gmail.com>
2021-09-18 15:22:14 +0200

    add recepte

--- api/main.py
+++ api/main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
+import json
 
 app = FastAPI()
 
-# NEW
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://127.0.0.1:8080",
-    "http://127.0.0.1:8081",
-    "http://localhost:8080",
-    "http://localhost:8080"],
+                   "http://127.0.0.1:8081",
+                   "http://localhost:8080",
+                   "http://localhost:8081"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,3 +20,11 @@ app.add_middleware(
 @app.get("/")
 def home():
     return "Hallo, Miriam!"
+
+
+@app.get("/recepten")
+def get_recepten():
+    with open('../data/recepten.json') as file:
+        data = json.load(file)
+
+    return data
